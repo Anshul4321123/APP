aad51e98ef50b903b237547b4312a5e33616915d
Anshul4321123 <anshul04072005@gmail.com>
2025-09-15 22:37:38 +0530

    change

--- FULLSTACK-RECIPE-APP/backend/drizzle.config.js
+++ FULLSTACK-RECIPE-APP/backend/drizzle.config.js
@@ -4,5 +4,5 @@ export default{
     schema:"./src/db/schema.js",
     out:"./src/db/migrations",
     dialect:"postgresql",
-    dbCredentials:{url: ENV.DATABASE_URL}
+    dbCredentials:{url: ENV.DATABASE_URL}  
 };
